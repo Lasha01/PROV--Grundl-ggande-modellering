5421a481fc617a9943d5cea8e589acf57cf9b2d9
Lasha01 <55137959+Lasha01@users.noreply.github.com>
2019-10-01 13:34:26 +0200

    Klar ish

---
+++ Antikvariatägarens Dotter/Antikvariatägarens Dotter/Book.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antikvariatägarens_Dotter
+{
+    class Book
+    {
+        public int Price;
+        string name;
+        int rarity;
+        string category;
+        int actualValue;
+        bool cursed;
+        static Random Generator = new Random(); 
+
+        public Book()
+        {
+            actualValue = Generator.Next(1001);
+            rarity = Generator.Next(11);
+            //cursed = Generator.Next();  Detta tog väldigt lång tid för mig att fösöka lista ut, måste träna
+
+            List<string> chooseCategory = new List<string>() { "PocketBook", "HugeBook", "ThinBook", "CoolBook" };
+            List<string> chooseName = new List<string>() { "LifeBook", "DeathBook", "SadBook", "HappyBook" };  // Måste träna på listor och hur man framkallar objekt ur listan
+            int category = Generator.Next(chooseCategory.Count);
+            int name = Generator.Next(chooseName.Count);
+            
+        }
+        public void PrintInfo()
+        {
+            Price = actualValue;
+            Console.WriteLine(category);
+            Console.WriteLine(name);
+            Console.WriteLine(rarity);
+            Console.WriteLine(Price);
+        }
+        public int Evaluate ()
+        {
+            int rightPrice = Price * rarity;
+            int randomprice = Generator.Next(50, 151);
+            rightPrice = randomprice / Price;
+            int evaluatedPrice = rightPrice * Price;
+
+            return evaluatedPrice;
+        }
+
+        public string GetCategory()
+
+        {
+
+        }
+
+    }
+}
---
+++ Antikvariatägarens Dotter/Antikvariatägarens Dotter/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antikvariatägarens_Dotter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book p = new Book();
+            
+           
+
+        }
+    }
+}
